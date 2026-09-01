ba1255d213f790298fb8b6ca272cc291fd3c1e21
DannyAJJ <klksistemas.app@gmail.com>
2026-09-01 17:28:47 -0400

    Integrador

--- IntegradorOdoo/query_odoo.sql
+++ IntegradorOdoo/query_odoo.sql
@@ -1,46 +1,70 @@
 -- Identificar el lote de facturas
-DECLARE @Pendientes TABLE (id Nvarchar(50));
-INSERT INTO @Pendientes SELECT TOP 100 NumFactura FROM (
-    SELECT NumFactura, 0 AS priority, FechaFactura
+-- CORRECCIÓN: Agregar IDSucursal a la tabla temporal
+DECLARE @Pendientes TABLE (IDSucursal Nvarchar(50), NumFactura Nvarchar(50));
+
+INSERT INTO @Pendientes (IDSucursal, NumFactura)
+SELECT TOP 100 IDSucursal, NumFactura FROM (
+    SELECT IDSucursal, NumFactura, 0 AS priority, FechaFactura
     FROM KLK_FACTURAHDR
     WHERE EnviadoASAP = 0 AND LogErrorIntegrador IS NULL
     UNION ALL
-    SELECT NumFactura, 1 AS priority, FechaFactura
+    SELECT IDSucursal, NumFactura, 1 AS priority, FechaFactura
     FROM KLK_FACTURAHDR
     WHERE EnviadoASAP = 0 AND LogErrorIntegrador IS NOT NULL
-) AS sub ORDER BY priority ASC, FechaFactura ASC;
+) AS sub 
+ORDER BY priority ASC, FechaFactura ASC;
 
 -- RESULTSET [0]: Cabeceras
 SELECT 
-    F.NumFactura AS id,
-    (SELECT TOP 1 T.ObjType FROM KLK_FACTURALINE L WITH (NOLOCK)
+    F.IDSucursal + '-' + F.NumFactura AS id,    
+    (
+     SELECT TOP 1 T.ObjType 
+     FROM KLK_FACTURALINE L WITH (NOLOCK)
      INNER JOIN KLK_TIPO_DOCUMENTO_AUDITORIA T WITH (NOLOCK) ON L.CodigoAlmacen = T.CodigoAlmacen
-     WHERE L.NumFactura = F.NumFactura) AS picking_type_id,
+     -- CORRECCIÓN: Agregar IDSucursal al cruce de la subconsulta
+     WHERE L.NumFactura = F.NumFactura AND L.IDSucursal = F.IDSucursal
+    ) AS picking_type_id,
     F.CodCliente AS partner_vat, C.NomCliente AS partner_name, C.Correo AS partner_email,
     C.Telefono AS partner_phone, C.Direccion AS partner_street,
     C.Agente, C.Tasa,
-    F.NumFactura AS external_ref, F.FechaFactura AS date, 
+    F.Error as serial_impresora,
+    F.IDSucursal + '-' + F.NumFactura AS external_ref, F.FechaFactura AS date, 
     F.TasaUSD as nx_rate, F.TasaUSD as currency_rate,
-    NumTicketFiscal as nx_nro_ctrl, NCorteZ as nx_fiscal_printer_invoice_number_z,
-    F.FacturaACredito, C.DiasCredito
+    F.NumTicketFiscal as nro_ctrl,
+    F.Error as nx_nro_ctrl, NCorteZ as nx_fiscal_printer_invoice_number_z,
+    F.FacturaACredito, 
+    IIF(F.FacturaACredito = 1, C.DiasCredito, 0) as DiasCredito
 FROM KLK_FACTURAHDR F WITH (NOLOCK)
 INNER JOIN KLK_CLIENTE C WITH (NOLOCK) ON F.CodCliente = C.CodCliente
-WHERE F.NumFactura IN (SELECT id FROM @Pendientes);
+-- CORRECCIÓN: Hacer JOIN con la tabla temporal usando ambas columnas
+INNER JOIN @Pendientes P ON F.IDSucursal = P.IDSucursal AND F.NumFactura = P.NumFactura;
+
 
 -- RESULTSET [1]: Líneas
-SELECT NumFactura, CodArticulo AS product_id, Cantidad AS qty,
+SELECT 
+    L.IDSucursal + '-' + L.NumFactura as NumFactura, 
+    L.CodArticulo AS product_id, 
+    L.Cantidad AS qty,
     -- TotalDespDescuentosUsd/Cantidad  AS price_unit  -- query en usd
-    TotalDespDescuentos/Cantidad  AS price_unit,  -- query en bs
-    CodigoAlmacen AS warehouse_code
-FROM KLK_FACTURALINE WITH (NOLOCK)
-WHERE NumFactura IN (SELECT id FROM @Pendientes);
+    L.TotalDespDescuentos/L.Cantidad  AS price_unit,  -- query en bs
+    L.CodigoAlmacen AS warehouse_code
+FROM KLK_FACTURALINE L WITH (NOLOCK)
+-- CORRECCIÓN: Hacer JOIN con la tabla temporal usando ambas columnas
+INNER JOIN @Pendientes P ON L.IDSucursal = P.IDSucursal AND L.NumFactura = P.NumFactura;
+
 
 -- RESULTSET [2]: Pagos
-SELECT CL.NFactura AS NumFactura, PC.IdFPagoSAP AS journal_id, PC.IdFPagoSAP AS journal_code, 
+SELECT 
+    CL.IDSucursal + '-' + CL.NFactura AS NumFactura, 
+    PC.CodigoCuenta as CodCuenta,
+    D.JournalId AS journal_id, 
+    D.JournalId AS journal_code, 
     -- CL.MontoUsd AS amount -- query en usd
     CL.Monto AS amount,  -- query en bs
     CL.FormadePago, CL.NTransaccion, CL.TarjetaCredito,
     CASE WHEN CL.FormadePago = 'Pago Nota de crédito' THEN 1 ELSE 0 END AS es_nota_credito
 FROM KLK_COBROLINE CL WITH (NOLOCK)
 JOIN KLK_SAP_PLANDECUENTA PC ON CL.CuentaSAP = PC.CodigoCuenta
-WHERE CL.NFactura IN (SELECT id FROM @Pendientes);
+LEFT JOIN KLK_SAP_DIARIO D ON PC.CodigoCuenta = D.CodigoCuenta
+-- CORRECCIÓN: Hacer JOIN con la tabla temporal usando ambas columnas
+INNER JOIN @Pendientes P ON CL.IDSucursal = P.IDSucursal AND CL.NFactura = P.NumFactura;
--- IntegradorOdoo/query_odoo_notas.sql
+++ IntegradorOdoo/query_odoo_notas.sql
@@ -1,35 +1,54 @@
 -- Identificar el lote de notas de credito
-DECLARE @Pendientes TABLE (id Nvarchar(50));
-INSERT INTO @Pendientes SELECT TOP 100 NumNota FROM (
-    SELECT NumNota, 0 AS priority, FechaNota
+-- CORRECCIÓN: Agregar IDSucursal a la tabla temporal
+DECLARE @Pendientes TABLE (IDSucursal Nvarchar(50), NumNota Nvarchar(50));
+
+INSERT INTO @Pendientes (IDSucursal, NumNota)
+SELECT TOP 100 IDSucursal, NumNota FROM (
+    SELECT IDSucursal, NumNota, 0 AS priority, FechaNota
     FROM KLK_NCREDITOHDR
     WHERE EnviadoASAP = 0 AND LogErrorIntegrador IS NULL
     UNION ALL
-    SELECT NumNota, 1 AS priority, FechaNota
+    SELECT IDSucursal, NumNota, 1 AS priority, FechaNota
     FROM KLK_NCREDITOHDR
     WHERE EnviadoASAP = 0 AND LogErrorIntegrador IS NOT NULL
-) AS sub ORDER BY priority ASC, FechaNota ASC;
+) AS sub 
+ORDER BY priority ASC, FechaNota ASC;
 
 -- RESULTSET [0]: Cabeceras
 SELECT 
-    F.NumNota AS id, F.facturaafectada,
-    (SELECT TOP 1 T.ObjType FROM KLK_NCREDITOLINE L WITH (NOLOCK)
+    -- CORRECCIÓN: Se concatena la sucursal para evitar IDs duplicados en el destino
+    F.IDSucursal + '-' + F.NumNota AS id, 
+    F.IDSucursal + '-' + F.facturaafectada AS facturaafectada,
+    (
+     SELECT TOP 1 T.ObjType 
+     FROM KLK_NCREDITOLINE L WITH (NOLOCK)
      INNER JOIN KLK_TIPO_DOCUMENTO_AUDITORIA T WITH (NOLOCK) ON L.CodigoAlmacen = T.CodigoAlmacen
-     WHERE L.NumNota = F.NumNota) AS picking_type_id,
+     -- CORRECCIÓN: Agregar IDSucursal al cruce de la subconsulta
+     WHERE L.NumNota = F.NumNota AND L.IDSucursal = F.IDSucursal
+    ) AS picking_type_id,
     F.CodCliente AS partner_vat, C.NomCliente AS partner_name, C.Correo AS partner_email,
     C.Telefono AS partner_phone, C.Direccion AS partner_street,
     C.Agente, C.Tasa, C.DiasCredito,
-    F.NumNota AS external_ref, F.FechaFactura AS date, 
+    F.Error as serial_impresora,
+    F.IDSucursal + '-' + F.NumNota AS external_ref, F.FechaFactura AS date, 
     F.TasaUSD as nx_rate, F.TasaUSD as currency_rate,
-    NumTicketFiscal as nx_nro_ctrl, NCorteZ as nx_fiscal_printer_invoice_number_z
+    F.NumTicketFiscal as nro_ctrl,
+    F.Error as nx_nro_ctrl,NCorteZ as nx_fiscal_printer_invoice_number_z
 FROM KLK_NCREDITOHDR F WITH (NOLOCK)
 INNER JOIN KLK_CLIENTE C WITH (NOLOCK) ON F.CodCliente = C.CodCliente
-WHERE F.NumNota IN (SELECT id FROM @Pendientes);
+-- CORRECCIÓN: Usar INNER JOIN con la tabla temporal (llave compuesta)
+INNER JOIN @Pendientes P ON F.IDSucursal = P.IDSucursal AND F.NumNota = P.NumNota;
+
 
 -- RESULTSET [1]: Líneas
-SELECT NumNota, CodArticulo AS product_id, Cantidad AS qty,
-    -- TotalDespDescuentosUsd/Cantidad  AS price_unit  -- query en usd
-    TotalDespDescuentos/Cantidad  AS price_unit,  -- query en bs
-    CodigoAlmacen AS warehouse_code
-FROM KLK_NCREDITOLINE WITH (NOLOCK)
-WHERE NumNota IN (SELECT id FROM @Pendientes);
+SELECT 
+    -- CORRECCIÓN: Concatenar la sucursal al igual que en facturas
+    L.IDSucursal + '-' + L.NumNota as NumNota, 
+    L.CodArticulo AS product_id, 
+    L.Cantidad AS qty,
+    -- L.TotalDespDescuentosUsd/L.Cantidad  AS price_unit  -- query en usd
+    L.TotalDespDescuentos/L.Cantidad  AS price_unit,  -- query en bs
+    L.CodigoAlmacen AS warehouse_code
+FROM KLK_NCREDITOLINE L WITH (NOLOCK)
+-- CORRECCIÓN: Usar INNER JOIN con la tabla temporal (llave compuesta)
+INNER JOIN @Pendientes P ON L.IDSucursal = P.IDSucursal AND L.NumNota = P.NumNota;
